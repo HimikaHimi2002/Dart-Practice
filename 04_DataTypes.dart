7709bf247e1c43f406af4c1ce62c8cb420063b63
HimikaHimi2002 <himikahimi2002@gmail.com>
2026-09-17 14:32:59 +0600

    Add Data Types practice

---
+++ 04_DataTypes.dart
@@ -0,0 +1,21 @@
+void main(){
+  String name = "Himika";
+  int age = 24;
+  double cgpa = 3.50;
+  bool isStudent = true;
+
+  List<String> subjects = ['Dart', 'Flutter'];
+
+  Map<String, String> info = {
+    'Roll': '21027',
+    'Department': 'CSE',
+    'Institute': 'IST'
+  };
+
+  print(name);
+  print(age);
+  print(cgpa);
+  print(isStudent);
+  print(subjects);
+  print(info);
+}
