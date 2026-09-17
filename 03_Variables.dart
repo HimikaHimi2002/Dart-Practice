eabe35892207639cca55ef2aa279275b0a8aa405
HimikaHimi2002 <himikahimi2002@gmail.com>
2026-09-17 14:09:41 +0600

    Add Variables practice

---
+++ 03_Variables.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+void main() {
+  String name = "Himika";
+  int age = 24;
+  double cgpa = 3.50;
+  bool isStudent = true;
+
+  List<String> subjects = ["Dart", "Flutter"];
+
+  Map<String, String> info = {
+    'Name': 'Himika',
+    'Department': 'CSE',
+    'Institute': 'IST'
+  };
+
+  print(name);
+  print(age);
+  print(cgpa);
+  print(isStudent);
+  print(subjects);
+  print(info);
+
+}
