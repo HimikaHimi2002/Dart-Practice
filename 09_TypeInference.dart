f1d6369bf183f0df4f341561d87975ab4fe79e23
HimikaHimi2002 <himikahimi2002@gmail.com>
2026-09-18 20:12:49 +0600

    Add TypeInference

---
+++ 09_TypeInference.dart
@@ -0,0 +1,17 @@
+void main() {
+  var name = "Himika";
+  var age = 24;
+  var cgpa = 3.50;
+  var isStudent = true;
+
+  print(name);
+  print(age);
+  print(cgpa);
+  print(isStudent);
+
+  //Dart Automatically understands the types
+  print(name.runtimeType);
+  print(age.runtimeType);
+  print(cgpa.runtimeType);
+  print(isStudent.runtimeType);
+}
