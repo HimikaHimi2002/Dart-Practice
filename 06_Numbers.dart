33efbbc030ef5814e7384ec852c63d1425bd305b
HimikaHimi2002 <himikahimi2002@gmail.com>
2026-09-18 12:49:22 +0600

    Add Numbers Practice

---
+++ 06_Numbers.dart
@@ -0,0 +1,24 @@
+void main() {
+  //Integer
+  int age = 24;
+  int BirthYear = 2002;
+
+  //Double
+  double cgpa = 3.50;
+
+  print(age);
+  print(BirthYear);
+  print(cgpa);
+
+  //Arithmetic Operator
+  print(10+10);
+  print(10-5);
+  print(10*8);
+  print(10/5);
+
+  //Integer Division
+  print(10 ~/ 3);
+
+  //Modulo/Remainder
+print(10%3);
+}
