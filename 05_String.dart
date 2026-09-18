e70a969111b5eaaa4ee98c4edc01a90a68246ed0
HimikaHimi2002 <himikahimi2002@gmail.com>
2026-09-18 12:17:27 +0600

    Add String Practice

---
+++ 05_String.dart
@@ -0,0 +1,26 @@
+void main() {
+  //String
+  String name = "Himika";
+  String message = "Welcome to Dart Programming";
+
+  print(name);
+  print(message);
+
+  //String Concatenation
+  String FirstName = "Himika";
+  String LastName = "Akter";
+print(FirstName + " " + LastName);
+
+//String Interpolation
+print("My name is $FirstName $LastName");
+
+//String Length
+print(name.length);
+
+//String to Uppercase
+print(name.toUpperCase());
+
+//String to Lowercase
+print(name.toLowerCase());
+
+}
