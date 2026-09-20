5edf68e33dc403241ca2a1a817806583bbea1596
HimikaHimi2002 <himikahimi2002@gmail.com>
2026-09-20 23:10:52 +0600

    Nested if else

---
+++ 15_Nested_if.dart
@@ -0,0 +1,17 @@
+void main() {
+    int age = 20;
+    bool hasID = true;
+  if (age >= 18)
+  {
+  if (hasID)
+  {
+      print("You Can Enter");
+  }
+  else {
+      print("You Need an ID");
+  }
+    }
+  else {
+      print("You are not Allowed!");
+    }
+  }
