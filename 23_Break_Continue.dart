35ea0672d0684b418892107d404c0b7a967e39e7
HimikaHimi2002 <himikahimi2002@gmail.com>
2026-09-22 23:38:07 +0600

    Break Continue

---
+++ 23_Break_Continue.dart
@@ -0,0 +1,23 @@
+void main(){
+  //Break Example
+  print("Break Example:");
+  for(var i = 1; i <= 10; i++)
+  {
+    if (i == 9)
+    {
+      break;
+    }
+      print(i);
+    }
+
+  //Continue Example
+  print("Continue Example:");
+  for(var i = 1; i<=10; i++)
+{
+  if(i == 9)
+    {
+      continue;
+    }
+  print(i);
+}
+  }
